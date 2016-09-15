53a796fdcd548477560b3bab5024e7c90111325f
Erik Humphrey <ContactErikH@gmail.com>
2016-09-15 17:06:53 -0400

    No longer closes pinned tabs

--- TabCloseScript.js
+++ TabCloseScript.js
@@ -1,16 +1,25 @@
-function closeLeftTabs() { 
-	var curTab;
-	chrome.tabs.getSelected( null , function(tab) {  
-		curTab=tab;
-	});
-	chrome.tabs.getAllInWindow(null,function(tabs) {
-		for (var i = 0; i < tabs.length; i++) {
-			if (tabs[i].index<curTab.index){
-				chrome.tabs.remove(tabs[i].id, null);
-			}
-		}
-	});
-} 
-chrome.browserAction.onClicked.addListener(function(tab) {    
-	  closeLeftTabs();
-  });
+chrome.browserAction.onClicked.addListener(function(tab) {  
+   closeLeftTabs();
+});
+
+
+function closeLeftTabs() {
+
+  performActionOnCurrentTab(function(activeTab){
+     if(activeTab) {
+        chrome.tabs.query({currentWindow : true,pinned: false},function(tabs) {
+             for (var i = 0; i < tabs.length; i++) {
+                 if (tabs[i].index < activeTab.index){
+                     chrome.tabs.remove(tabs[i].id, null);
+                 }
+             }
+         });
+     }
+ });
+
+
+}
+
+function performActionOnCurrentTab(tabCallback) {
+   chrome.tabs.query({ currentWindow : true, active : true },function (tabArray) { tabCallback(tabArray[0]); });
+}
