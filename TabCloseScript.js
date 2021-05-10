ebeffc468156f1b34354bc97b05a6b4c40aa8cd3
Erik Humphrey <erik.humphrey@carleton.ca>
2021-05-09 21:36:38 -0400

    Remove tabs permission and update to Manifest V3

--- TabCloseScript.js
+++ TabCloseScript.js
@@ -1,25 +1,21 @@
-chrome.browserAction.onClicked.addListener(function(tab) {  
-   closeLeftTabs();
+chrome.action.onClicked.addListener(function (tab) {
+    closeLeftTabs();
 });
 
-
 function closeLeftTabs() {
-
-  performActionOnCurrentTab(function(activeTab){
-     if(activeTab) {
-        chrome.tabs.query({currentWindow : true,pinned: false},function(tabs) {
-             for (var i = 0; i < tabs.length; i++) {
-                 if (tabs[i].index < activeTab.index){
-                     chrome.tabs.remove(tabs[i].id, null);
-                 }
-             }
-         });
-     }
- });
-
-
+    performActionOnCurrentTab(function (activeTab) {
+        if (activeTab) {
+            chrome.tabs.query({ currentWindow: true, pinned: false }, function (tabs) {
+                for (var i = 0; i < tabs.length; i++) {
+                    if (tabs[i].index < activeTab.index) {
+                        chrome.tabs.remove(tabs[i].id, null);
+                    }
+                }
+            });
+        }
+    });
 }
 
 function performActionOnCurrentTab(tabCallback) {
-   chrome.tabs.query({ currentWindow : true, active : true },function (tabArray) { tabCallback(tabArray[0]); });
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabArray) { tabCallback(tabArray[0]); });
 }
